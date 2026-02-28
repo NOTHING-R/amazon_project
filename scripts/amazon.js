b8fcd8e7d2518e4bce1305d303942a68bb12fb27
NOTHING-R <kalilinux.nothing@gmail.com>
2026-02-28 15:31:28 +0600

    first-js-changing

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    ratings: {
-        stars: 4.5,
-        count: 87,
-    },
-    priceCent: 1090,
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    ratings: {
-        stars: 4,
-        count: 127,
-    },
-    priceCent: 2095,
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    ratings: {
-        stars: 4.5,
-        count: 56,
-    },
-    priceCent: 799,
-}]
-
 let productsHtml = '';
 products.forEach((product) => {
-    productsHtml += `
+  productsHtml += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -39,14 +13,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${(product.ratings.stars * 10)}.png">
+              src="images/ratings/rating-${(product.rating.stars) * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.ratings.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCent / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -75,8 +49,8 @@ products.forEach((product) => {
             Add to Cart
           </button>
         </div>
-`;
-}) 
+    `;
+})
 
 
 document.querySelector('.js-product-grid').innerHTML = productsHtml;
